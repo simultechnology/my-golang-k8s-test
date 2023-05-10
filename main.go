782a83b7c153efff6a9de6a65ec390622ae393db
takatsugu.ishikawa <tissi0708@gmail.com>
2023-05-11 01:10:56 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"path/filepath"
+)
+
+func main() {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// uses the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//namespaces, err := clientset.CoreV1().Namespaces().List(context.TODO(), v1.ListOptions{})
+	//if err != nil {
+	//	panic(err.Error())
+	//}
+	//for _, ns := range namespaces.Items {
+	//	fmt.Printf("namespace: %s\n", ns.Name)
+	//}
+
+	// get pods in all the namespaces by omitting namespace
+	// Or specify namespace to get pods in particular namespace
+	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), v1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+	// list all pods
+	for _, pod := range pods.Items {
+		fmt.Printf("Namespace: %s, Pod Name: %s\n", pod.Namespace, pod.Name)
+	}
+}
